069a40b1513a9c34e2bec1441fce4253e2359338
Hardik Swarnkar <hardikswarnkar123@gmail.com>
2022-04-26 16:45:50 +0530

    Updates

---
+++ precache-manifest.9e452aa151fd461f4040d6c7c4aaa7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360a8fd4cf2e09b2a778a8d92a70377b",
+    "url": "/COVID-19-TRACKER/index.html"
+  },
+  {
+    "revision": "ef8c18571a78aea3b8ed",
+    "url": "/COVID-19-TRACKER/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "6dfc66f9a6ce2fea2a80",
+    "url": "/COVID-19-TRACKER/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "ef8c18571a78aea3b8ed",
+    "url": "/COVID-19-TRACKER/static/js/2.6e0db766.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/COVID-19-TRACKER/static/js/2.6e0db766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dfc66f9a6ce2fea2a80",
+    "url": "/COVID-19-TRACKER/static/js/main.1cf8cd3b.chunk.js"
+  },
+  {
+    "revision": "7d2df0ca2ef4e72d1c5a",
+    "url": "/COVID-19-TRACKER/static/js/runtime-main.68e40c04.js"
+  }
+]);
